fd73d705c990cc4f67e4de24bd7fac672538336d
Ostrov1717 <ostrov1717@gmail.com>
2026-06-07 23:12:05 +0300

    Task_12

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "cmtr-yz7bruip-backend-new-bucket-1780824830"
-    key    = "tf_code.tfstate"
-    region = "eu-west-1"
+    bucket = ""
+    key    = ""
+    region = ""
   }
 }
--- resources.tf
+++ resources.tf
@@ -1,12 +1,15 @@
-resource "aws_iam_policy" "custom_policy" {
-  name        = "resource-move-demo-policy"
+resource "aws_iam_policy" "this" {
+  name        = "cmtr-yz7bruip-iam-policy"
   description = "Custom role with limited permissions"
+
   policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
       {
-        Effect   = "Allow",
-        Action   = ["ec2:*", "s3:*"], # Replace with specific actions
+        Effect = "Allow"
+        Action = [
+          "ec2:*",
+        "s3:*"],
         Resource = "*"
       }
     ]
